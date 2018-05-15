9de5a8442b99474a6144ff900ba7764bdbc640c7
ravi4all <ravikanttyagi.2295@gmail.com>
2018-05-15 14:59:34 +0530

    Add files via upload

--- Javascript/MusicPlayer/script.js
+++ Javascript/MusicPlayer/script.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", initEvent);
 
 var audio;
-
+var slider;
 function initEvent(){
     audio = document.getElementById("audio");
+    slider = document.getElementById("slider");
+    slider.addEventListener("click",seekSong);
+    document.getElementById("playSong").addEventListener("click",startSong);
+    document.getElementById("pauseSong").addEventListener("click",pauseSong);
     printSongs();
 }
 
@@ -18,10 +22,17 @@ function printSongs(){
 
     songsArray.forEach(function(obj){
         var li = document.createElement('li');
-        li.setAttribute('title',obj.song_id);
-        li.innerHTML = obj.song_name;
+        var span = document.createElement("span");
+        span.innerHTML = obj.song_name;
+        span.setAttribute('title',obj.song_id);
+        var play_list_btn = document.createElement("button");
+        play_list_btn.innerHTML = "Add to Playlist";
+        play_list_btn.className = 'btn btn-primary playList_btn';
+        li.appendChild(span);
+        li.appendChild(play_list_btn);
         songList.appendChild(li);
-        li.addEventListener("click", playSong);
+        span.addEventListener("click", playSong);
+        play_list_btn.addEventListener("click",addSong);
     })
 
 }
@@ -37,5 +48,44 @@ function playSong(){
         }
     });
     audio.src = songUrl;
-    audio.play()
+    audio.play();
+    setTimeout(function(){
+        slider.max = audio.duration;
+    },100);
+    setInterval(function(){
+        slider.value = audio.currentTime;
+    },1000);
+}
+
+function seekSong(){
+    audio.currentTime = slider.value;
+}
+
+function addSong(){
+    var ul = document.getElementById("playList")
+    var song_to_add = event.srcElement.parentElement;
+    // console.log(li);
+    var li = document.createElement("li");
+    li.innerHTML = song_to_add.innerHTML;
+    console.log(li.children);
+    var span = li.children[0];
+    var btn = li.children[1];
+    // console.log(btn);
+    btn.innerHTML = "Delete Song";
+    ul.appendChild(li);
+    btn.addEventListener("click",deleteSong);
+    span.addEventListener("click",playSong);
+}
+
+function deleteSong(){
+    var li = event.srcElement.parentElement;
+    li.style.display = 'none';
+}
+
+function startSong(){
+    audio.play();
+}
+
+function pauseSong(){
+    audio.pause();
 }
